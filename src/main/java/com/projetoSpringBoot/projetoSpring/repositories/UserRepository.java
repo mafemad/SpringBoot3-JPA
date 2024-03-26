7eebf8e54e7e11f29544d215baccfae99be9a385
Mateus Madeira <mafemad2005@gmail.com>
2024-03-26 09:28:43 -0300

    camada de serviço e registro de componentes

--- src/main/java/com/projetoSpringBoot/projetoSpring/repositories/UserRepository.java
+++ src/main/java/com/projetoSpringBoot/projetoSpring/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projetoSpringBoot.projetoSpring.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
