e14d4cb2fe6a7937c57912a85424d083134ea176
Mateus Madeira <mafemad2005@gmail.com>
2024-03-28 15:54:29 -0300

    Product

---
+++ src/main/java/com/projetoSpringBoot/projetoSpring/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.projetoSpringBoot.projetoSpring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projetoSpringBoot.projetoSpring.entities.Product;
+import com.projetoSpringBoot.projetoSpring.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
+
