a9c374db79378ccdecace753bad928052cd55427
Mateus Madeira <mafemad2005@gmail.com>
2024-04-02 17:24:07 -0300

    Exception handling

--- src/main/java/com/projetoSpringBoot/projetoSpring/services/UserService.java
+++ src/main/java/com/projetoSpringBoot/projetoSpring/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projetoSpringBoot.projetoSpring.entities.User;
 import com.projetoSpringBoot.projetoSpring.repositories.UserRepository;
+import com.projetoSpringBoot.projetoSpring.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
